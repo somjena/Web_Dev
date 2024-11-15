8129aa72ce8af49937bba67b6b32192495c53309
Soumitra jena <131749408+somjena@users.noreply.github.com>
2024-11-15 13:10:43 +0530

    Added the Animation

---
+++ Jspider_clone/script.js
@@ -0,0 +1,16 @@
+gsap.from("#logo",{
+  opacity:0,
+  duration:2,
+  delay:1
+})
+gsap.from("#slogan",{
+  opacity:0,
+  color:"orange",
+  duration:2,
+  delay:1
+})
+gsap.from("#nav-items",{
+  opacity:0,
+  duration:2,
+  delay:1
+})
